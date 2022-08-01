e3c2e8b171a8885000aa5832d8b9f4c6613440b6
Douglas Rios <glinhas55@hotmail.com>
2022-08-01 16:54:16 -0300

    continuando com o desenvolvimento, 2 etapas prontas

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Project_Feed_Whats_Help
+
+-Parei com 23:19 do segundo video
+https://www.youtube.com/watch?v=CbY7TA5y5aU&list=PLEJMI9SZSMK6caK8HnJ-plUCHSrmPNSWA&index=2&t=1268s
--- web/src/components/WidgetButton.tsx
+++ web/src/components/WidgetButton.tsx
@@ -1,13 +1,17 @@
 import { ChatTeardropDots } from 'phosphor-react';
 import { Popover } from '@headlessui/react';
+import WidgetForm from './WidgetForm';
 
-export function Widget() {
+export function WidgetButton() {
     return (
-        <Popover className="absolute bottom-5 right-5">
-            <Popover.Panel>Hello Word</Popover.Panel>
+        <Popover className="absolute bottom-4 right-4 md:right-8 md:right-8 flex flex-col items-end">
+            {/* botão que abre o pop na tela, ele vai renderizar o forms */}
+            <Popover.Panel>
+                <WidgetForm />
+            </Popover.Panel>
             <Popover.Button className="bg-[#8257E6] rounded-full px-3 h-12 text-white flex items-center group">
+                {/* Chat que mostra quando o hover é acionado */}
                 <ChatTeardropDots className="w-6 h-6" />
-
                 <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
                     <span className="pl-2"></span>
                     Feedback
