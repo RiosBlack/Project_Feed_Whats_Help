4c2c17f70d4787a8f4d2a6534c6c1a2426c642d9
Douglas Rios <glinhas55@hotmail.com>
2022-08-05 12:12:43 -0300

    implementando os buttons

--- web/src/components/WidgetForm.tsx
+++ web/src/components/WidgetForm.tsx
@@ -1,11 +1,58 @@
+import CloseButton from './CloseButton';
+
+import BugImageUrl from '../assets/bug.png';
+import LampImageUrl from '../assets/lamp.png';
+import OtherImageUrl from '../assets/outro.png';
+
+const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: BugImageUrl,
+            alt: 'Imagem de um inseto',
+        },
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: LampImageUrl,
+            alt: 'Imagem de uma lâmpada',
+        },
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: OtherImageUrl,
+            alt: 'Imagem de três pontos',
+        },
+    },
+};
+
 export default function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            Hello word
             <header>
                 <span className="text-xl leading-6">Deixe seu feedback</span>
+                <CloseButton />
             </header>
-            <p>Hello word</p>
+
+            <div className="flex py-8 gap-2 w-full">
+                {Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col itens-center gap-2 border-2 border-transparent hover:border-[#8257E6] focus:border-[#8257E6] focus:outline-none"
+                            type="button"
+                        >
+                            <img
+                                src={value.image.source}
+                                alt={value.image.alt}
+                            />
+                            <span>{value.title}</span>
+                        </button>
+                    );
+                })}
+            </div>
+
             <footer className="text-xs text-neutral-400">
                 Feito por Douglas Rios -{' '}
                 <a
