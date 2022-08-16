287801399d8b4f0d7aed7be5c0857256b4e24b74
Douglas Rios <glinhas55@hotmail.com>
2022-08-16 08:59:03 -0300

    Ajustes do backend deploy

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,2 +1,19 @@
 "use strict";
-const teste = 1;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const routes_1 = require("./routes");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+//iniciando o servidor com o express
+const app = (0, express_1.default)();
+//para fazer um controle de quais os frontend pode acessar o backend
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+//importando as rotas
+app.use(routes_1.routes);
+//chamando a porta e passando uma função para executar assim que liberar a porta.
+app.listen(3333, () => {
+    console.log('Servidor iniciado com sucesso');
+});
